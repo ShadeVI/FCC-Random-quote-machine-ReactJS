81ec33ef1f4e02c31afaacd43afefb6221b0b4b1
ShadeVI <manuel.t.shade@gmail.com>
2021-03-30 01:51:42 +0100

    added link to github

--- src/App.js
+++ src/App.js
@@ -1,43 +1,45 @@
-import { useState, useEffect, useCallback } from 'react';
-import Card from './Card';
+import { useState, useEffect, useCallback } from "react";
+import Card from "./Card";
 
 const backgrounds = ["#b6cf86", "#ccb98f", "#66babd", "#8bbde8", "#e3caed"];
-const url = "https://type.fit/api/quotes"
+const url = "https://type.fit/api/quotes";
 
 const bodyEl = document.querySelector("body");
 
 function App() {
   const [quote, setQuote] = useState({});
   const [color, setColor] = useState(null);
-  const [isError, setIsError] = useState(false)
+  const [isError, setIsError] = useState(false);
 
   const changeColor = () => {
-    const randIdx = Math.floor(Math.random() * backgrounds.length)
-    bodyEl.style.backgroundColor = backgrounds[randIdx]
-    setColor(backgrounds[randIdx])
-  }
+    const randIdx = Math.floor(Math.random() * backgrounds.length);
+    bodyEl.style.backgroundColor = backgrounds[randIdx];
+    setColor(backgrounds[randIdx]);
+  };
 
   const fetchQuote = useCallback(async () => {
-    setIsError(false)
+    setIsError(false);
     try {
       const response = await fetch(url);
       const data = await response.json();
-      const randIdx = Math.floor(Math.random() * data.length)
-      setQuote(data[randIdx])
-      changeColor()
+      const randIdx = Math.floor(Math.random() * data.length);
+      setQuote(data[randIdx]);
+      changeColor();
     } catch (err) {
-      setIsError(true)
+      setIsError(true);
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
     fetchQuote();
-  }, [fetchQuote])
+  }, [fetchQuote]);
 
   if (isError) {
-    return <div className="d-flex justify-content-center mt-5">
-      <h4>OPPPS, something went wrong</h4>
-    </div>
+    return (
+      <div className="d-flex justify-content-center mt-5">
+        <h4>OPPPS, something went wrong</h4>
+      </div>
+    );
   }
 
   return (
@@ -45,6 +47,14 @@ function App() {
       <h1 className="text-center">FreeCodeCamp - FrontEnd Libraries</h1>
       <h2 className="text-center mt-3">Random Quote Machine</h2>
       <Card quote={quote} newQuote={fetchQuote} newColor={color} />
+      <footer className="fixed-bottom text-center mb-2">
+        <small>
+          Developed by{" "}
+          <a className="link-primary" href="https://github.com/ShadeVI">
+            ShadeVI
+          </a>
+        </small>
+      </footer>
     </div>
   );
 }
